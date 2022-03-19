0815a6590302d4b5ac2de765e1923ee3e2d01c17
vocu <101651423+vocu@users.noreply.github.com>
2022-03-19 11:01:02 +0100

    Change naming in help.go

--- help.go
+++ help.go
@@ -16,10 +16,10 @@ var ColorCommand = Yellow
 var ColorArgs = Magenta
 
 // Usage prints usage text to os.Stdout.
-func (parser *command) Usage() {
+func (c *command) Usage() {
 	Println(ColorMenu+"Usage:"+End, 0)
 	s := strings.Repeat(" ", GlossaryOffset)
-	s += parser.Name + " "
+	s += c.Name + " "
 
 	// TEST
 	//s += "[" + ColorOption + "options" + End + "] [" + ColorCommand + "command" + End + "] [" + ColorArgs + "args" + End + "]"
@@ -29,28 +29,28 @@ func (parser *command) Usage() {
 	//return
 	// TEST END
 
-	if parser.usgShortFlags != "" {
-		s += "[" + ColorFlag + "-" + parser.usgShortFlags + End + "] "
+	if c.usgShortFlags != "" {
+		s += "[" + ColorFlag + "-" + c.usgShortFlags + End + "] "
 	}
 
-	s += parser.usgFlags
+	s += c.usgFlags
 
 	if !NoHelp {
 		s += "[" + ColorFlag + "--" + "help" + End + "] "
 	}
-	if parser.Version != "" {
+	if c.Version != "" {
 		s += "[" + ColorFlag + "--" + "version" + End + "] "
 	}
 
-	s += parser.usg
+	s += c.usg
 
-	if parser.Meta != "" {
-		s += parser.Meta + " "
+	if c.Meta != "" {
+		s += c.Meta + " "
 	} else {
-		if len(parser.commands) > 0 {
+		if len(c.commands) > 0 {
 			s += "<" + ColorCommand + "command" + End + "> ... "
 		} else {
-			if parser.MinArgs > 0 {
+			if c.MinArgs > 0 {
 				s += ColorArgs + "<args>" + End + "... "
 			} else {
 				s += "[" + ColorArgs + "<args>" + End + "]... "
@@ -63,89 +63,89 @@ func (parser *command) Usage() {
 }
 
 // Glossary prints glossary to os.Stdout.
-func (parser *command) Glossary() {
-	if parser.Version != "" && parser.longest == 0 {
-		parser.longest = len("--version")
+func (c *command) Glossary() {
+	if c.Version != "" && c.longest == 0 {
+		c.longest = len("--version")
 	}
-	if !NoHelp && parser.longest == 0 {
-		parser.longest = len("--help")
+	if !NoHelp && c.longest == 0 {
+		c.longest = len("--help")
 	}
 
 	Println(ColorMenu+"Options: "+End, 0)
 
 	// dynamic length of 3 spaces to print if there exists at least one optin/flag with long name
 	dyn := 0
-	if parser.gotAbbr {
-		parser.longest += 3
+	if c.gotAbbr {
+		c.longest += 3
 		dyn = 3
 	}
 	// End Find
 
 	// Print Flags -a
-	for i := range parser.flags {
+	for i := range c.flags {
 		s := strings.Repeat(" ", GlossaryOffset)
 
-		if parser.flags[i].abbr != "" && parser.flags[i].name == "" {
-			s += ColorFlag + "-" + parser.flags[i].abbr + End
-			s += strings.Repeat(" ", parser.longest-2)
-			s += "   " + parser.flags[i].help
+		if c.flags[i].abbr != "" && c.flags[i].name == "" {
+			s += ColorFlag + "-" + c.flags[i].abbr + End
+			s += strings.Repeat(" ", c.longest-2)
+			s += "   " + c.flags[i].help
 			Println(s, GlossaryOffset)
 		}
 	}
 
 	// Print Options -a=XXX
-	for i := range parser.options {
+	for i := range c.options {
 		s := strings.Repeat(" ", GlossaryOffset)
-		if parser.options[i].abbr != "" && parser.options[i].name == "" {
-			s += ColorOption + "-" + parser.options[i].abbr + End + "=" + ColorMeta + parser.options[i].meta + End
-			s += strings.Repeat(" ", parser.longest-2-1-len(parser.options[i].meta))
-			s += "   " + parser.options[i].help
+		if c.options[i].abbr != "" && c.options[i].name == "" {
+			s += ColorOption + "-" + c.options[i].abbr + End + "=" + ColorMeta + c.options[i].meta + End
+			s += strings.Repeat(" ", c.longest-2-1-len(c.options[i].meta))
+			s += "   " + c.options[i].help
 			Println(s, GlossaryOffset)
 		}
 	}
 
 	// Print Flags -a|--aa
-	for i := range parser.flags {
+	for i := range c.flags {
 		s := strings.Repeat(" ", GlossaryOffset)
-		if parser.flags[i].abbr != "" && parser.flags[i].name != "" {
-			s += ColorFlag + "-" + parser.flags[i].abbr + End + "|" + ColorFlag + "--" + parser.flags[i].name + End
-			s += strings.Repeat(" ", parser.longest-dyn-1-len(parser.flags[i].abbr)-len(parser.flags[i].name))
-			s += "   " + parser.flags[i].help
+		if c.flags[i].abbr != "" && c.flags[i].name != "" {
+			s += ColorFlag + "-" + c.flags[i].abbr + End + "|" + ColorFlag + "--" + c.flags[i].name + End
+			s += strings.Repeat(" ", c.longest-dyn-1-len(c.flags[i].abbr)-len(c.flags[i].name))
+			s += "   " + c.flags[i].help
 			Println(s, GlossaryOffset)
 		}
 	}
 
 	// Print Options -a|--aa=XXX
-	for i := range parser.options {
+	for i := range c.options {
 		s := strings.Repeat(" ", GlossaryOffset)
-		if parser.options[i].abbr != "" && parser.options[i].name != "" {
-			s += ColorOption + "-" + parser.options[i].abbr + End + "|" + ColorOption + "--" + parser.options[i].name + End + "=" + ColorMeta + parser.options[i].meta + End
-			s += strings.Repeat(" ", parser.longest-dyn-1-len(parser.options[i].abbr)-len(parser.options[i].name)-1-len(parser.options[i].meta))
-			s += "   " + parser.options[i].help
+		if c.options[i].abbr != "" && c.options[i].name != "" {
+			s += ColorOption + "-" + c.options[i].abbr + End + "|" + ColorOption + "--" + c.options[i].name + End + "=" + ColorMeta + c.options[i].meta + End
+			s += strings.Repeat(" ", c.longest-dyn-1-len(c.options[i].abbr)-len(c.options[i].name)-1-len(c.options[i].meta))
+			s += "   " + c.options[i].help
 			Println(s, GlossaryOffset)
 		}
 	}
 
 	// Print Options --aa=XXX
-	for i := range parser.options {
+	for i := range c.options {
 		s := strings.Repeat(" ", GlossaryOffset)
-		if parser.options[i].abbr == "" && parser.options[i].name != "" {
+		if c.options[i].abbr == "" && c.options[i].name != "" {
 			s += strings.Repeat(" ", dyn)
-			s += ColorOption + "--" + parser.options[i].name + End + "=" + ColorMeta + parser.options[i].meta + End
-			s += strings.Repeat(" ", parser.longest-dyn-2-len(parser.options[i].name)-1-len(parser.options[i].meta))
-			s += "   " + parser.options[i].help
+			s += ColorOption + "--" + c.options[i].name + End + "=" + ColorMeta + c.options[i].meta + End
+			s += strings.Repeat(" ", c.longest-dyn-2-len(c.options[i].name)-1-len(c.options[i].meta))
+			s += "   " + c.options[i].help
 			Println(s, GlossaryOffset)
 		}
 	}
 
 	// Flags just --long without -a
-	for i := range parser.flags {
+	for i := range c.flags {
 		s := strings.Repeat(" ", GlossaryOffset)
-		if parser.flags[i].abbr == "" && parser.flags[i].name != "" {
+		if c.flags[i].abbr == "" && c.flags[i].name != "" {
 			s += strings.Repeat(" ", dyn)
-			s += ColorFlag + "--" + parser.flags[i].name + End
-			s += strings.Repeat(" ", parser.longest-dyn-2-len(parser.flags[i].name))
-			s += "   " + parser.flags[i].help
+			s += ColorFlag + "--" + c.flags[i].name + End
+			s += strings.Repeat(" ", c.longest-dyn-2-len(c.flags[i].name))
+			s += "   " + c.flags[i].help
 			Println(s, GlossaryOffset)
 		}
 	}
@@ -154,16 +154,16 @@ func (parser *command) Glossary() {
 		s := strings.Repeat(" ", GlossaryOffset)
 		s += strings.Repeat(" ", dyn)
 		s += ColorFlag + "--help" + End
-		s += strings.Repeat(" ", parser.longest-dyn-len("--help"))
+		s += strings.Repeat(" ", c.longest-dyn-len("--help"))
 		s += "   " + "Show this help"
 		Println(s, GlossaryOffset)
 	}
 
-	if parser.Version != "" {
+	if c.Version != "" {
 		s := strings.Repeat(" ", GlossaryOffset)
 		s += strings.Repeat(" ", dyn)
 		s += ColorFlag + "--version" + End
-		s += strings.Repeat(" ", parser.longest-dyn-len("--version"))
+		s += strings.Repeat(" ", c.longest-dyn-len("--version"))
 		s += "   " + "Show version information"
 		Println(s, GlossaryOffset)
 	}
@@ -171,15 +171,15 @@ func (parser *command) Glossary() {
 }
 
 // Commands prints command help text to os.Stdout.
-func (parser *command) Commands() {
-	if len(parser.commands) > 0 {
+func (c *command) Commands() {
+	if len(c.commands) > 0 {
 		Println(ColorMenu+"Commands: "+End, 0)
-		for i := range parser.commands {
+		for i := range c.commands {
 			s := strings.Repeat(" ", GlossaryOffset)
 			//s += strings.Repeat(" ", dyn)
-			s += ColorCommand + parser.commands[i].Name + End
-			s += strings.Repeat(" ", parser.longestCommand-len(parser.commands[i].Name))
-			s += "   " + parser.commands[i].Short
+			s += ColorCommand + c.commands[i].Name + End
+			s += strings.Repeat(" ", c.longestCommand-len(c.commands[i].Name))
+			s += "   " + c.commands[i].Short
 			Println(s, GlossaryOffset)
 		}
 		fmt.Println()
@@ -187,24 +187,24 @@ func (parser *command) Commands() {
 }
 
 // Help prints help text to os.Stdout and exits.
-func (parser *command) Help() {
+func (c *command) Help() {
 
 	// Print Description
-	if parser.Long != "" {
-		Println(parser.Long, 0)
+	if c.Long != "" {
+		Println(c.Long, 0)
 	}
-	if parser.Long != "" {
+	if c.Long != "" {
 		fmt.Println()
 	}
 
-	parser.Usage()
+	c.Usage()
 	fmt.Println()
-	parser.Commands()
-	parser.Glossary()
+	c.Commands()
+	c.Glossary()
 
-	if len(parser.commands) > 0 && !NoHelp {
+	if len(c.commands) > 0 && !NoHelp {
 		fmt.Println()
-		Println("Run \""+parser.Name+" <command> --help\" for more information on a command.", 0)
+		Println("Run \""+c.Name+" <command> --help\" for more information on a command.", 0)
 	}
 
 	os.Exit(0)
